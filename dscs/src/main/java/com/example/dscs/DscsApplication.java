dd1ff5caa6eaae8dd1f64240efe7f8c20afdb493
Muhammad Baba Muhammad <59345145+mohbaba@users.noreply.github.com>
2024-12-03 11:13:20 +0100

    Initial Commit

---
+++ dscs/src/main/java/com/example/dscs/DscsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscsApplication.class, args);
+	}
+
+}
---
+++ dscs/src/test/java/com/example/dscs/DscsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
